0bccee6e1722f9ad3040fe9823dfada432b0615d
Vi1ctor <82913507+Vi1ctor@users.noreply.github.com>
2024-10-02 22:00:44 -0300

    Add files via upload
    teste

---
+++ player_2.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+var  SPEED: int  = 360 
+var  Y_MAX: int  = 52
+var  Y_MIN: int  = 666
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_down_paddle_right"):
+		position.y -= SPEED * delta
+	if Input.is_action_pressed("move_up_paddle_right"):
+		position.y += SPEED * delta
+		
+	position.y	= clamp(position.y,Y_MAX, Y_MIN)
